dd90629b1ade21cb77fd7552b82a3e90ea174be2
l06in <mudin.imam24@gmail.com>
2024-03-30 07:50:47 +0700

    create layout in view

--- Tugas 2/ci4 news/app/Config/Routes.php
+++ Tugas 2/ci4 news/app/Config/Routes.php
@@ -5,7 +5,5 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/About', 'Page::About');
-$routes->get('/Contact', 'Page::Contact');
-$routes->get('/Faqs', 'Page::Faqs');
+$routes->get('/', 'Pages/Home::index');
+
